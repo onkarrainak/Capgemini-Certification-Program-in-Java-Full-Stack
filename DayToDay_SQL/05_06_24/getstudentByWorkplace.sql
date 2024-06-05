a6b0557e4608f1ba5f4c904a167af364d8b570fa
onkarrainak <rainakonkar@gmail.com>
2024-06-05 15:24:10 +0530

    add files

--- DayToDay_SQL/05_06_24/getstudentByWorkplace.sql
+++
@@ -1,4 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getstudentByWorkplace`(in get_by_Workplace varchar(10))
-BEGIN
-SELECT * FROM student WHERE get_by_Workplace = Workplace;
-END
--- DayToDay_SQL/05_06_24/getstudentByrollno.sql
+++
@@ -1,4 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getstudentByrollno`(in get_by_rollno int)
-BEGIN
-SELECT * FROM student WHERE get_by_rollno = rollno;
-END
