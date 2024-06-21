0322754384ba95698389a749bc886f836bca92ae
onkarrainak <rainakonkar@gmail.com>
2024-06-21 21:33:18 +0530

    Spring initializr

---
+++ Spring/springprojectinitializr/springprojectinitializr/src/main/java/com/spring/springprojectinitializr/SpringprojectinitializrApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springprojectinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectinitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectinitializrApplication.class, args);
+	}
+
+}
---
+++ Spring/springprojectinitializr/springprojectinitializr/src/test/java/com/spring/springprojectinitializr/SpringprojectinitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springprojectinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectinitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
